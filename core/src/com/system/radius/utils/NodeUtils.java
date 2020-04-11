6a167d8cb75effe3a98619b24f88c192096f4be8
system-radius <radius.sairo@gmail.com>
2020-04-11 21:16:01 +0800

    #5 (upd)
     Adds a method for removal of duplicates from a list based on another
     list.

--- core/src/com/system/radius/utils/NodeUtils.java
+++ core/src/com/system/radius/utils/NodeUtils.java
@@ -4,6 +4,9 @@ import com.system.radius.ai.Node;
 import com.system.radius.objects.AbstractBomberObject;
 import com.system.radius.objects.board.BoardState;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NodeUtils {
 
   private static BoardState boardState = BoardState.getInstance();
@@ -35,6 +38,47 @@ public class NodeUtils {
   public static Node createNode(int x, int y) {
 
     return new Node(null, x, y, 0, 0);
+  }
+
+  /**
+   * Remove the duplicates from the base list. The indicated duplicates are from another list.
+   *
+   * @param baseList    - The base list from which the duplicates are to be removed.
+   * @param toBeRemoved - The list containing the items to be removed.
+   * @return The new list with the duplicates removed.
+   */
+  public static List<Node> removeDuplicates(List<Node> baseList, List<Node> toBeRemoved) {
+
+    List<Node> nodeList = new ArrayList<>();
+
+    for (Node node : baseList) {
+
+      if (findInList(toBeRemoved, node)) {
+        // Skip adding the node that was found in the list that is to be removed.
+        continue;
+      }
+
+      // Add the node as it does not have a duplicate in the to be removed list.
+      nodeList.add(node);
+    }
+
+    return nodeList;
+  }
+
+  public static boolean findInList(List<Node> list, Node node) {
+
+    String nodeString = node.toString();
+    for (Node inList : list) {
+
+      String temp = inList.toString();
+      if (temp.equals(nodeString)) {
+        // If the node has an equal string, then it exists in the list.
+        return true;
+      }
+
+    }
+
+    return false;
   }
 
 }
