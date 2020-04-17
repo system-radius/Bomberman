603dfa2ed6a02af2754a3cba4d833cf807c6519c
system-radius <radius.sairo@gmail.com>
2020-04-18 00:33:29 +0800

    #4 #12 (upd)
     Loads the blocks sprite sheet as static;
     Provides initialize method to customize the sprites that the blocks
     will use;
     Removes usage of delta time when drawing;
     Applies logic to the draw method.

--- core/src/com/system/radius/objects/blocks/Block.java
+++ core/src/com/system/radius/objects/blocks/Block.java
@@ -2,7 +2,9 @@ package com.system.radius.objects.blocks;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Disposable;
@@ -14,13 +16,20 @@ import java.util.List;
 
 public class Block extends AbstractBomberObject implements Disposable {
 
+  public static final Texture BLOCKS_SPRITE_SHEET = new Texture("neko/img/newBricks.png");
+
+  protected static final TextureRegion[][] REGIONS =
+      TextureRegion.split(BLOCKS_SPRITE_SHEET, 32, 32);
+
   protected final static long DESTROY_TIMER = 1000;
+
+  protected Animation<TextureRegion> animation;
 
   protected List<Player> players;
 
-  protected Texture spriteSheet;
-
   protected Rectangle bounds;
+
+  protected Texture spriteSheet;
 
   protected boolean activeCollision;
 
@@ -41,10 +50,20 @@ public class Block extends AbstractBomberObject implements Disposable {
     bounds = new Rectangle(x, y, width, height);
     activeCollision = true;
 
+    initialize();
   }
 
   public Block(float x, float y, float width, float height) {
     this(WorldConstants.BOARD_PERMA_BLOCK, x, y, width, height);
+  }
+
+  protected void initialize() {
+
+    TextureRegion[] frames = new TextureRegion[1];
+    frames[0] = REGIONS[0][5];
+
+    animation = new Animation<>(0, frames);
+
   }
 
 //  public void collide(Player player) {
@@ -95,12 +114,14 @@ public class Block extends AbstractBomberObject implements Disposable {
   }
 
   @Override
-  public void draw(Batch batch, float delta) {
+  public void draw(Batch batch) {
+
+    batch.draw(animation.getKeyFrames()[0], getX(), getY(), width, height);
 
   }
 
   @Override
-  public void drawDebug(ShapeRenderer shapeRenderer, float delta) {
+  public void drawDebug(ShapeRenderer shapeRenderer) {
 
     shapeRenderer.setColor(Color.WHITE);
     shapeRenderer.rect(getX(), getY(), getWidth(), getHeight());
