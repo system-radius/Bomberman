ecb75516789ecf586b00bd266de9b702cee0b4a0
system-radius <radius.sairo@gmail.com>
2020-04-19 11:42:29 +0800

    #2 #12 (upd)
     Updates the sprite loading logic;
     Removes usage of millis;
     Removes update override;
     Adds color to track power ups in debug mode.

--- core/src/com/system/radius/objects/blocks/SoftBlock.java
+++ core/src/com/system/radius/objects/blocks/SoftBlock.java
@@ -2,23 +2,22 @@ package com.system.radius.objects.blocks;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.system.radius.objects.board.BoardState;
 import com.system.radius.objects.board.WorldConstants;
+import com.system.radius.utils.FieldConfig;
 
 public class SoftBlock extends Block {
 
-  public SoftBlock(float x, float y, float width, float height) {
-    super(WorldConstants.BOARD_SOFT_BLOCK, x, y, width, height);
+  public SoftBlock(float x, float y, float width, float height, boolean hasBonus) {
+    super(WorldConstants.BOARD_SOFT_BLOCK, x, y, width, height, hasBonus);
     this.life = 1;
   }
 
   @Override
   protected void initialize() {
     TextureRegion[] frames = new TextureRegion[3];
-    System.arraycopy(REGIONS[0], 0, frames, 0, 3);
+    System.arraycopy(REGIONS[FieldConfig.getFieldIndex()], 0, frames, 0, 3);
 
     animation = new Animation<>(1f / 3f, frames);
   }
@@ -33,40 +32,13 @@ public class SoftBlock extends Block {
     life--;
     if (life == 0) {
       burning = true;
-      burnStart = System.currentTimeMillis();
     }
-  }
-
-  @Override
-  public void update(float delta) {
-
-    if (!burning) {
-      return;
-    }
-
-    animationElapsedTime += delta;
-    if (System.currentTimeMillis() - burnStart >= DESTROY_TIMER) {
-      BoardState.getInstance().removeFromBoard(this);
-    }
-
-  }
-
-  @Override
-  public void draw(Batch batch) {
-
-    if (!burning) {
-      batch.draw(animation.getKeyFrames()[0], getX(), getY(), getWidth(), getHeight());
-      return;
-    }
-
-    batch.draw(animation.getKeyFrame(animationElapsedTime), getX(), getY(), getWidth(), getHeight());
-
   }
 
   @Override
   public void drawDebug(ShapeRenderer shapeRenderer) {
 
-    shapeRenderer.setColor(Color.MAGENTA);
+    shapeRenderer.setColor(hasBonus ? Color.GOLD : Color.MAGENTA);
 
     if (burning) {
       shapeRenderer.setColor(Color.RED);
